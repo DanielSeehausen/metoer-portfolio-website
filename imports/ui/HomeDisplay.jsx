a6818fae6a7521ce1ee169028fc90dd62716da50
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2016-12-12 22:14:50 -0500

    removed personal information

--- imports/ui/HomeDisplay.jsx
+++ imports/ui/HomeDisplay.jsx
@@ -5,15 +5,16 @@ import Projects from './Projects.js';
 // Project description component - represents a single project description
 export default class HomeDisplay extends Component {
   
-  //we are wrapping the homeDisplay in the projDescription container div because it occupies the same space
+  //we are wrapping the homeDisplay in the projDescription container div because of their overlap
+  //If desired, this class could be scrapped all together and the homepage could be a ProjectDescription itself.
   render() {
     return (
       <div className="projDescription" id="homeDisplay">
         <div id="homeDisplayText">
-          <h1>I'm Daniel, a programmer and designer</h1>
+          <h1>I'm XXXXX, a programmer and designer</h1>
           <h2>Please take a look at some of my work</h2>
-          <a href="mailto:daniel.seehausen@gmail.com">Daniel.Seehausen@gmail.com</a>
-          <h3>(484) 883-7551</h3>
+          <a href="mailto:google.google@gmail.com">XXXXX.XXXXXX@XXXXXX.com</a>
+          <h3>(XXX) XXX-XXXX</h3>
         </div>
       </div>
     );
